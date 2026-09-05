a95026693f0f45ab46cb4ce010b56d51ed29937d
y-song <y.song@berkeley.edu>
2026-09-04 17:45:41 -0700

    Start working with trackEfficiency workflow

---
+++ run_track_qa.sh
@@ -0,0 +1,9 @@
+# OPTION_TUTORIAL="-b --configuration json://myConfig.json"
+# o2-analysistutorial-mm-my-example-task ${OPTION_TUTORIAL}
+
+OPTION="-b --configuration json://753916_config.json"
+o2-analysis-trackselection ${OPTION} | \
+o2-analysis-propagationservice ${OPTION} | \
+o2-analysis-event-selection-service ${OPTION} | \
+o2-analysis-je-jet-deriveddata-producer ${OPTION} | \
+o2-analysis-je-track-efficiency ${OPTION} --aod-file 559613/AO2D_013.root --shm-segment-size 16000000000 --min-failure-level error
